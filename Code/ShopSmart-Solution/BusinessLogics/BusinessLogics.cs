3b0bc1e3ec6dc421f3a79b5e075f690638e94ccb
Avi <avi.turner111@gmail.com>
2013-04-27 21:42:19 +0300

    Added code- ZFR

---
+++ Code/ShopSmart-Solution/BusinessLogics/BusinessLogics.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ShopSmart.Dal;
+
+
+namespace ShopSmart.Bl
+{
+    /// <summary>
+    /// Handles All logics related to Shop lists
+    /// </summary>
+    public class BusinessLogics:IDisposable
+    {
+
+        /// <summary>
+        /// The data base that we are working against
+        /// </summary>
+        ShopSmartEntities _db;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BusinessLogics"/> class.
+        /// </summary>
+        public BusinessLogics()
+        {
+            //Initializing the DAL
+            this._db = new ShopSmartEntities();
+            //we will have to detect and save changes manually
+            this._db.Configuration.AutoDetectChangesEnabled = false;
+            this._db.Configuration.LazyLoadingEnabled = true;
+            this._db.Configuration.ProxyCreationEnabled= true;
+            this._db.Configuration.ValidateOnSaveEnabled= true;
+        }
+
+
+        /// <summary>
+        /// Sorts the specified shop list.
+        /// </summary>
+        /// <param name="shoppingList">The shopping list.</param>
+        private void SortShopList(ShopList shoppingList)
+        {
+            //if we got a null, there is nothing for us to do
+            if (shoppingList != null)
+            {
+                //the ordered items in
+                IOrderedEnumerable<ShoplistItem> orderedItems = shoppingList.ShoplistItems.OrderBy(item => item.Product.Category.Id);
+                shoppingList.ShoplistItems = orderedItems.ToList<ShoplistItem>();
+                
+
+            }
+           
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            try
+            {
+                this._db.Dispose();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                //nothing to do here
+                //TODO: Add log
+            }
+        }
+    }
+}
---
+++ Code/ShopSmart-Solution/ShopSmart/ClientForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ShopSmart.Common;
+
+namespace ShopSmart.Client
+{
+    public partial class ClientForm : Form
+    {
+        public ClientForm()
+        {
+            InitializeComponent();
+            this.SetIcon();
+        }
+
+        private void SetIcon()
+        {
+            //System.Resources.ResourceManager resources =
+            //                        new System.Resources.ResourceManager(typeof(Utils));
+
+            //this.Icon = (Icon)resources.GetObject("Clock");
+        }
+    }
+}
