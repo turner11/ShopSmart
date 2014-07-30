0cbd9cfc4fbee425058d47d666ee424fe4e3e4b8
Avi <avi.turner111@gmail.com>
2014-07-30 07:37:11 +0300

    attempting to get JS working...

--- Code/ShopSmart-Solution/ShopSmart.Web.MVC/App_Start/BundleConfig.cs
+++ Code/ShopSmart-Solution/ShopSmart.Web.MVC/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace ShopSmart.Web.MVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //Minifying etc...
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
