2c406dd8a0a8bc21de80d57b402a0bb2a34428ba
Avi Turner <avi.turner111@gmail.com>
2014-08-03 08:04:31 +0300

    work on posting json
    still need to post + redirect

--- Code/ShopSmart-Solution/ShopSmart.Web.MVC/Scripts/ViewsScripts/CreateList.js
+++ Code/ShopSmart-Solution/ShopSmart.Web.MVC/Scripts/ViewsScripts/CreateList.js
@@ -1,23 +1,30 @@
-﻿
-$(document).ready(function() {
-    TableToJson();
-});
-
-function TableToJson() {
+﻿function TableToJson() {
     debugger
     // Loop through grabbing everything
     var myRows = [];
     var $headers = $("th");
     var $rows = $("tbody tr").each(function(index) {
         $cells = $(this).find("td");
-        myRows[index] = {};
-        $cells.each(function(cellIndex) {
-            myRows[index][$($headers[cellIndex]).html()] = $(this).html();
-        });    
+        var $chkToBuy = $cells.find(".chkToBuy");
+        if ($chkToBuy.length >0 && $chkToBuy[0].checked) {
+    
+        
+            myRows[index] = {}; 
+            $cells.each(function (cellIndex) {
+                var prodNameIdx = 2;
+                //for somewht of optimization get only fields that are editable => input
+                //or will be used for identification
+                $inputs = $(this).find('input');
+                if ($inputs.length > 0 || cellIndex == prodNameIdx) {
+                    myRows[index][$($headers[cellIndex]).html()] = $(this).html();
+
+                }
+            });  
+        }
     });
 
     // Let's put this in the object like you want and convert to JSON (Note: jQuery will also do this for you on the Ajax request)
     var myObj = {};
     myObj.myrows = myRows;
-   // alert(JSON.stringify(myObj));​
+   //alert(JSON.stringify(myObj));​
 }
